4062e6a67e400db442c37b7795ed7f47b2bc132e
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-05-13 22:52:17 +0500

    refactor: clean up code structure and improve readability in sample.cpp

--- exp9/sample.cpp
+++ exp9/sample.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include <stdio.h>      
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,68 +6,49 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 #include <semaphore.h>
+using namespace std;
 
+//Create a program where a parent and a child process both increment and print a shared counter variable. Use shared memory for the counter and use a semaphore to ensure mutual exclusion so that the parent and child do not access the counter at the same time.
 
-// Create a program where a parent and a child process both increment and print a shared counter variable. Use shared memory for the counter and use a semaphore to ensure mutual exclusion so that the parent and child do not access the counter at the same time.
+int main () {
 
-int main(int argc, char **argv) {
-    int i, nloop = 10, *ptr;
-    int shmid1, shmid2;
-    int SHMSIZE = sizeof(int);  
-    int SEMSIZE = sizeof(sem_t); 
-    sem_t *p_mutex;
+    int shmid1 = shmget(IPC_PRIVATE,sizeof(int),IPC_CREAT | 0666);
 
-    if ((shmid1 = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | 0666)) < 0) {
-        perror("shmget for counter");
-        exit(1);
-    }
-    ptr = (int *)shmat(shmid1, NULL, 0);
-    if (ptr == (int *)-1) {
-        perror("shmat for counter");
-        exit(1);
-    }
-    *ptr = 0; // Initialize counter
+    int *ptr = (int *)shmat(shmid1,NULL,0);
 
-    // Shared memory for the semaphore
-    if ((shmid2 = shmget(IPC_PRIVATE, SEMSIZE, IPC_CREAT | 0666)) < 0) {
-        perror("shmget for semaphore");
-        exit(1);
-    }
+    *ptr = 0;   
 
-    p_mutex = (sem_t *)shmat(shmid2, NULL, 0);
-    if (p_mutex == (sem_t *)-1) {
-        perror("shmat for semaphore");
-        exit(1);
-    }
+    int shmid2 = shmget(IPC_PRIVATE,sizeof(sem_t),IPC_CREAT | 0666);
 
-    
-    if (sem_init(p_mutex, 1, 1) == -1) {
-        perror("sem_init");
-        exit(1);
-    }
+    sem_t* p_mutex = (sem_t*) shmat(shmid2,NULL,0);
 
-    if (fork() == 0) {
-        // Child process
+    sem_init(p_mutex,1,1);  // If 1: shared between multiple processes (must be in shared memory)
+    // If 0: semaphore is shared between threads of the same process
+
+    if(fork() == 0) {
+
         sem_wait(p_mutex);
-        for (i = 0; i < nloop; i++)
-            printf("child: %d\n", (*ptr)++);
+        for(int i =0;i<10;i++){
+            cout << "child : " << ((*ptr)++) << endl;
+        }
         sem_post(p_mutex);
 
         shmdt(ptr);
         shmdt(p_mutex);
         exit(0);
-    }
 
+    }
     sem_wait(p_mutex);
-    for (i = 0; i < nloop; i++)
-        printf("parent: %d\n", (*ptr)++);
+    for(int i =0;i<10;i++){
+        cout << "parent : " << ((*ptr)++) << endl;
+    }
     sem_post(p_mutex);
 
-    wait(NULL); 
+    wait(NULL);
 
     sem_destroy(p_mutex);
-    shmctl(shmid1, IPC_RMID, NULL);
-    shmctl(shmid2, IPC_RMID, NULL);
+    shmctl(shmid1,IPC_RMID,NULL);
+    shmctl(shmid2,IPC_RMID,NULL);
     shmdt(ptr);
     shmdt(p_mutex);
 
